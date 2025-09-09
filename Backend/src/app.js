20829b9b1162a1f7e901ac31fb54a769bba025a3
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-09-09 09:28:30 +0530

    Feat: Added healthcheck route and controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,4 +3,10 @@ import express from "express";
 const app = express();
 
 
+// Import routes 
+import healthCheckRoutes from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRoutes);
+
+
 export default app;
---
+++ Backend/src/controllers/healthcheck.controller.js
@@ -0,0 +1,5 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+export const healthCheck = (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+};
